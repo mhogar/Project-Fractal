6c0c5283ac4bf13aca7996dba7244d707e7efd51
Ryan Stamp <mhogar98@gmail.com>
2018-11-10 18:57:57 -0500

    Added completed checkbox and dynamic styling and percen calculation

--- Task Tracker/app.js
+++ Task Tracker/app.js
@@ -2,8 +2,18 @@ var taskComponent = {
 	props: ['task'],
 	template: `
 						<div class="ui segments">
-							<div class="ui yellow segment">
-								<i class="thumbtack icon"></i>{{task.name}}
+							<div v-bind:class="'ui segment ' + (task.completed ? 'green task-complete' : 'yellow task-todo')">
+								<div class="ui grid">
+									<div class="left floated seven wide column">
+										<i class="thumbtack icon"></i>{{task.name}}
+									</div>
+									<div class="right floated three wide column">
+										<div class="ui checkbox">
+											<input type="checkbox" v-on:click="$parent.$parent.toggleDone($event, task.id)" v-bind:checked="task.completed">
+											<label>Completed</label>
+										</div>
+									</div>
+								</div>
 							</div>
 						</div>
 						`
@@ -18,7 +28,7 @@ var storyComponent = {
 						<div class="story-segment ui segments">
 							<div class="story-segment-header ui purple segment">
 								<div class="ui accordion">
-									<div class="title">
+									<div class="title active">
 										<div class="ui grid">
 											<div class="left floated seven wide column">
 												<i class="dropdown icon"></i>								
@@ -34,7 +44,7 @@ var storyComponent = {
 											</div>
 										</div>
 									</div>
-									<div class="content">
+									<div class="content active">
 										<Task v-for="task in $parent.getStoryTasks(story.id)" v-bind:task="task" />
 										<div class="ui segments">
 											<div class="ui segment">
@@ -51,14 +61,14 @@ var storyComponent = {
 //=============================================================================
 
 var taskData = [
-	{ id: 1, storyId: 1, name: "Task 1" },
-	{ id: 1, storyId: 1, name: "Another Task" },
-	{ id: 1, storyId: 2, name: "One More Task" }
+	{ id: 1, storyId: 1, name: "Task 1", completed: false },
+	{ id: 2, storyId: 1, name: "Another Task", completed: true },
+	{ id: 3, storyId: 2, name: "One More Task", completed: false }
 ];
 
 var storyData = [
-	{ id: 1, name: "Story", percent: 47},
-	{ id: 2, name: "Story 2", percent: 80}
+	{ id: 1, name: "Story", percent: 0},
+	{ id: 2, name: "Story 2", percent: 0}
 ];
 
 //=============================================================================
@@ -76,11 +86,41 @@ var storyData = [
 		getStoryTasks: function(storyId) {
 			return this.tasks.filter(task => task.storyId === storyId);
 		},
-		updateProgressBars: function() {
-			this.stories.forEach(function(story) {
-				let progressBar = $('#story-progress-bar-' + story.id);
-				progressBar.css('width', story.percent + '%');
-			});
+		updateProgressBars: function(storyId = false) {
+			let stories = this.stories;
+			let getStoryTasks = this.getStoryTasks;
+
+			let updateProgressBar = function(storyId) {
+				let story = stories.find(item => item.id === storyId);
+				if (story) {
+					tasks = getStoryTasks(story.id);
+					story.percent = tasks.filter(task => task.completed === true).length / tasks.length * 100;
+
+					let progressBar = $('#story-progress-bar-' + story.id);
+					progressBar.css('width', story.percent + '%');
+				}
+			};
+
+			if (storyId !== false) {
+				updateProgressBar(storyId);
+			}
+			else
+			{
+				this.stories.forEach(function(story) {
+					updateProgressBar(story.id);
+				});
+			}
+			
+		},
+		toggleDone: function (event, id) {
+			event.stopImmediatePropagation();
+
+			let task = this.tasks.find(item => item.id === id);
+			if (task) {
+				task.completed = !task.completed;
+
+				this.updateProgressBars(task.storyId);
+			}
 		}
 	},
 	mounted: function() {
